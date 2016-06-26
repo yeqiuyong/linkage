4e194c644639df38429d930062069ba96b75a939
wangyang66 <646617779@qq.com>
2016-06-26 15:31:23 +0800

    edit backend Contact

--- app/models/Contact.php
+++ app/models/Contact.php
@@ -24,6 +24,49 @@ class Contact extends Model
 		$this->setSource("linkage_contact");
 	}
 
+	public function getContact4Admin($pagination, $offset, $size){
+		$contacts = self::find([
+			'conditions' => 'status != :status:',
+			'bind' => ['status' => StatusCodes::NOTICE_DELETE],
+		]);
+
+		$results = [];
+		foreach ($contacts as $contact) {
+			$result = [];
+			$result['id'] = $contact->id;
+			$result['name'] = $contact->name;
+			$result['telephone'] = $contact->telephone;
+			$result['email'] = $contact->email;
+			$result['create_time'] = $contact->create_time;
+			$result['comments'] = $contact->comments;
+			$result['status'] = $contact->status;
+
+			array_push($results,$result);
+		}
+
+		return $results;
+	}
+
+	public function getContactDetail4Admin($contactId){
+		$contact = self::findFirst([
+			'conditions' => 'id = :contact_id:',
+			'bind' => ['contact_id' => $contactId]
+		]);
+
+		if(isset($contact->id)){
+			return [
+				'id' => $contact->id,
+				'name' => $contact->name,
+				'telephone' => $contact->telephone,
+				'email' => $contact->email,
+				'comments' => $contact->comments,
+				'create_time' => $contact->create_time,
+			];
+		}else{
+			throw new DataBaseException(ErrorCodes::DATA_FIND_FAIL, ErrorCodes::$MESSAGE[ErrorCodes::DATA_FIND_FAIL]);
+		}
+	}
+
 	public function add($name, $mobile, $email, $comments)
 	{
 		$now = time();
@@ -42,6 +85,31 @@ class Contact extends Model
 				$message .= (String)$msg . ",";
 			}
 
+			$logger = Di::getDefault()->get(Services::LOGGER);
+			$logger->fatal($message);
+
+			throw new DataBaseException(ErrorCodes::DATA_FAIL, ErrorCodes::$MESSAGE[ErrorCodes::DATA_FAIL]);
+		}
+	}
+
+	public function updateStatus($conId, $status){
+		$contact = self::findFirst([
+			'conditions' => 'id = :id:',
+			'bind' => ['id' => $conId]
+		]);
+
+		if(!isset($contact->id)){
+			throw new UserOperationException(ErrorCodes::USER_ADVERTISE_NOT_FOUND, ErrorCodes::$MESSAGE[ErrorCodes::USER_ADVERTISE_NOT_FOUND]);
+		}
+
+		$contact->status = $status;
+		$contact->update_time = time();
+
+		if($contact->update() == false){
+			$message = '';
+			foreach ($contact->getMessages() as $msg) {
+				$message .= (String)$msg. ',';
+			}
 			$logger = Di::getDefault()->get(Services::LOGGER);
 			$logger->fatal($message);
 
