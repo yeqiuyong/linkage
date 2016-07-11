67906c211dcf13fc426547184a65f44d0e2eaea6
wangyang66 <646617779@qq.com>
2016-07-11 09:45:14 +0800

    add port field

--- app/models/OrderExport.php
+++ app/models/OrderExport.php
@@ -49,7 +49,7 @@ class OrderExport extends Model
     }
 
     public function getDetail4Manufacture($orderId){
-        $phql="select a.transporter_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, b.name as company_name, c.so, c.so_images, c.ship_company, c.ship_name, c.ship_schedule_no, c.is_book_cargo from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderExport c where a.transporter_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
+        $phql="select a.transporter_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, b.name as company_name, c.port, c.so, c.so_images, c.ship_company, c.ship_name, c.ship_schedule_no, c.is_book_cargo from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderExport c where a.transporter_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
         $order = $this->modelsManager->executeQuery($phql);
 
         if(sizeof($order) == 0){
@@ -63,6 +63,7 @@ class OrderExport extends Model
             'take_time' => $order[0]->take_time,
             'delivery_address' => $order[0]->delivery_address,
             'delivery_time' => $order[0]->delivery_time,
+            'port' => $order[0]->port,
             'is_transfer_port' => $order[0]->is_transfer_port,
             'memo' => $order[0]->memo,
             'so' => $order[0]->so,
@@ -77,7 +78,7 @@ class OrderExport extends Model
     }
 
     public function getDetail4Transporter($orderId){
-        $phql="select a.manufacture_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, b.name as company_name, c.so, c.so_images, c.ship_company, c.ship_name, c.ship_schedule_no, c.is_book_cargo from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderExport c where a.manufacture_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
+        $phql="select a.manufacture_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, b.name as company_name, c.port, c.so, c.so_images, c.ship_company, c.ship_name, c.ship_schedule_no, c.is_book_cargo from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderExport c where a.manufacture_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
         $order = $this->modelsManager->executeQuery($phql);
 
         if(sizeof($order) == 0){
@@ -91,6 +92,7 @@ class OrderExport extends Model
             'take_time' => $order[0]->take_time,
             'delivery_address' => $order[0]->delivery_address,
             'delivery_time' => $order[0]->delivery_time,
+            'port' => $order[0]->port,
             'is_transfer_port' => $order[0]->is_transfer_port,
             'memo' => $order[0]->memo,
             'so' => $order[0]->so,
