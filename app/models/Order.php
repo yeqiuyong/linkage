2171c899312299908f5fe9595aaaa31228a9505e
wangyang66 <646617779@qq.com>
2016-07-23 13:47:44 +0800

    edit order search status

--- app/models/Order.php
+++ app/models/Order.php
@@ -151,9 +151,9 @@ class Order extends Model
         }
 
         if($status == 1){
-            $condition .= " and a.status in (0, 1, 2)";
+            $condition .= " and a.status in (0, 1)";
         }else if($status == 2){
-            $condition .= " and a.status in (3, 4)";
+            $condition .= " and a.status in (2, 3)";
         }else{
             $condition .= " and a.status not in (5)";
         }
@@ -194,9 +194,9 @@ class Order extends Model
         }
 
         if($status == 1){
-            $condition .= " and (a.status in (1, 2) or a.status in (0))";
+            $condition .= " and (a.status in (1) or a.status in (0))";
         }else if($status == 2){
-            $condition .= " and a.status in (3, 4)";
+            $condition .= " and a.status in (2, 3)";
         }else{
             $condition .= " and a.status not in (5)";
         }
