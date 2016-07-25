1d6fe89213197f8d31f59e6e0781d0ea7722298f
wangyang66 <646617779@qq.com>
2016-07-25 09:16:14 +0800

    edit status

--- app/models/Order.php
+++ app/models/Order.php
@@ -249,13 +249,13 @@ class Order extends Model
             }
             $driver_id = substr($driver_id,0,-1);
             //$driver_id=$plist[0]->driver_id;
-            $sqlcondition = " a.manufacture_id = $companyId and c.driver_id in(".$driver_id.")";
+            $sqlcondition = " a.manufacture_id = $companyId and c.driver_id in(".$driver_id.") and a.status not in(5) ";
 
             $phql="select DISTINCT a.order_id, a.type, a.create_time, a.update_time, a.status, a.transporter_id as company_id, b.name as company_name from Multiple\Models\Order a inner join Multiple\Models\Company b on a.transporter_id = b.company_id inner join Multiple\Models\DriverTask c on a.order_id = c.order_id where ". $sqlcondition." order by a.create_time desc ".$limit;
 
 
         }else{
-            $condition = "a.manufacture_id = $companyId";
+            $condition = "a.manufacture_id = $companyId and a.status not in(5) ";
 
             if($searchType == 0){
                 $condition .= " and a.order_id like '%".$value."%'";
@@ -313,13 +313,13 @@ class Order extends Model
             }
             $driver_id = substr($driver_id,0,-1);
             //$driver_id=$plist[0]->driver_id;
-            $sqlcondition = " a.transporter_id = $companyId and c.driver_id in(".$driver_id.")";
+            $sqlcondition = " a.transporter_id = $companyId and c.driver_id in(".$driver_id.") and a.status not in(5) ";
 
             $phql="select DISTINCT a.order_id, a.type, a.create_time, a.update_time, a.status, a.manufacture_id as company_id, b.name as company_name from Multiple\Models\Order a inner join Multiple\Models\Company b on a.manufacture_id = b.company_id inner join Multiple\Models\DriverTask c on a.order_id = c.order_id where ". $sqlcondition." order by a.create_time desc ".$limit;
 
 
         }else{
-            $condition = "a.transporter_id = $companyId";
+            $condition = "a.transporter_id = $companyId and a.status not in(5) ";
 
             if($searchType == 0){
                 $condition .= " and a.order_id like '%".$value."%'";
