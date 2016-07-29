414e41eb97e2f8a3f14cbc28affe9d0958e316df
wangyang66 <646617779@qq.com>
2016-07-29 23:35:32 +0800

    add comment field

--- app/models/OrderImport.php
+++ app/models/OrderImport.php
@@ -47,7 +47,7 @@ class OrderImport extends Model
 
 
     public function getDetail4Manufacture($orderId){
-        $phql="select a.transporter_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, b.name as company_name, c.cargos_rent_expire, c.bill_no, c.cargo_no, c.cargo_company, c.customs_broker, c.customshouse_contact from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderImport c where a.transporter_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
+        $phql="select a.transporter_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, a.is_comment, b.name as company_name, c.cargos_rent_expire, c.bill_no, c.cargo_no, c.cargo_company, c.customs_broker, c.customshouse_contact from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderImport c where a.transporter_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
         $order = $this->modelsManager->executeQuery($phql);
 
         if(sizeof($order) == 0){
@@ -69,13 +69,14 @@ class OrderImport extends Model
             'cargo_company' => $order[0]->cargo_company,
             'customs_broker' => $order[0]->customs_broker,
             'customs_contact' => $order[0]->customshouse_contact,
+            'is_comment' => $order[0]->is_comment,
         ];
 
         return $orderDetail;
     }
 
     public function getDetail4Transporter($orderId){
-        $phql="select a.manufacture_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, b.name as company_name, c.cargos_rent_expire, c.bill_no, c.cargo_no, c.cargo_company, c.customs_broker, c.customshouse_contact from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderImport c where a.manufacture_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
+        $phql="select a.manufacture_id, a.take_address, a.take_time, a.delivery_address, a.delivery_time, a.is_transfer_port, a.memo, a.is_comment, b.name as company_name, c.cargos_rent_expire, c.bill_no, c.cargo_no, c.cargo_company, c.customs_broker, c.customshouse_contact from Multiple\Models\Order a join Multiple\Models\Company b join Multiple\Models\OrderImport c where a.manufacture_id = b.company_id and a.order_id = c.order_id and a.order_id = '$orderId'";
         $order = $this->modelsManager->executeQuery($phql);
 
         if(sizeof($order) == 0){
@@ -97,6 +98,7 @@ class OrderImport extends Model
             'cargo_company' => $order[0]->cargo_company,
             'customs_broker' => $order[0]->customs_broker,
             'customs_contact' => $order[0]->customshouse_contact,
+            'is_comment' => $order[0]->is_comment,
         ];
 
         return $orderDetail;
