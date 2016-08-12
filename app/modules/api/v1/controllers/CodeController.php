fb70d78978ea1f6c9e1b292236872310e898c98b
wangyang66 <646617779@qq.com>
2016-08-12 11:49:16 +0800

    add alidayu

--- app/modules/api/v1/controllers/CodeController.php
+++ app/modules/api/v1/controllers/CodeController.php
@@ -27,12 +27,15 @@ class CodeController extends APIControllerBase
 
     private $sms;
 
+    private $alidayu;
+
     public function initialize(){
         parent::initialize();
 
         $this->redis = Di::getDefault()->get(Services::REDIS);
         $this->logger = Di::getDefault()->get(Services::LOGGER);
         $this->sms = Di::getDefault()->get(Services::SMS);
+        $this->alidayu = Di::getDefault()->get(Services::ALIDAYU);
     }
 
     /**
@@ -57,9 +60,10 @@ class CodeController extends APIControllerBase
 
             //如果客户端多次调用接口生成校验码，以最后一次校验码为准
             $this->redis->setex($key, $expire, $verify_code);
-
+            $this->alidayu->sendSMS($mobile, $verify_code);
+            //return $this->respondError('a', $a);
             //send message
-            $this->sms->send($mobile, $msg);
+            //$this->sms->send($mobile, $msg);
 
         }catch (Exception $e){
             return $this->respondError($e->getCode(), $e->getMessage());
