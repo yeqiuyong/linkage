979ede0525d6280b9d7e4a887e23e3ea13ec4c96
wangyang66 <646617779@qq.com>
2016-08-12 11:47:39 +0800

    add smsdaemon

---
+++ app/tasks/AlidayuTask.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 22/2/16
+ * Time: 10:37 PM
+ */
+
+require APP_PATH . 'app/core/libraries/Alidayu/TopSdk.php';
+require APP_PATH . 'app/core/libraries/Alidayu/top/TopClient.php';
+require APP_PATH . 'app/core/libraries/Alidayu/top/request/AlibabaAliqinFcSmsNumSendRequest.php';
+date_default_timezone_set('Asia/Shanghai');
+
+use Phalcon\Cli\Task;
+use Phalcon\Di;
+
+use Multiple\Core\Constants\Services;
+
+
+class AlidayuTask extends Task
+{
+
+    private $redis;
+
+    public function mainAction()
+    {
+        echo "\nThis is the default task and the default action \n";
+    }
+
+    /**
+     * @param array $params
+     */
+    public function sendAction()
+    {
+
+        $this->redis = Di::getDefault()->get(Services::REDIS);
+
+        while (true) {
+
+            $sms_json = $this->redis->rPop('alidayusms');
+            if(!$sms_json) {
+                sleep(2);
+                continue;
+            }
+
+            $sms = json_decode($sms_json, true);
+            if (!isset($sms['mobile']) || trim($sms['mobile']) == '') {
+                echo "Error: mobile number is null \n";
+                sleep(2);
+                continue;
+            }
+
+            $mobile = $sms['mobile'];
+            $code = $sms['code'];
+
+            echo "send $mobile push message \n";
+
+            $c = new \TopClient;
+            $c->appkey = '23428422';
+            $c->secretKey = 'baa9273882d94b1a7caad3e396341751';
+            $req = new \AlibabaAliqinFcSmsNumSendRequest;
+            $req->setExtend("123456");
+            $req->setSmsType("normal");
+            $req->setSmsFreeSignName("领骐物流");
+            $req->setSmsParam("{\"code\":\"$code\"}");
+            $req->setRecNum($mobile);
+            $req->setSmsTemplateCode("SMS_12991382");
+            $resp = $c->execute($req);
+
+            echo "\n\n";
+        }
+    }
+}
