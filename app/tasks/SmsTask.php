c3047bc5510b144489ca0a0eab7bf603b304574e
wangyang66 <646617779@qq.com>
2016-08-12 12:24:37 +0800

    edit sms

--- app/tasks/SmsTask.php
+++ app/tasks/SmsTask.php
@@ -6,67 +6,40 @@
  * Time: 10:37 PM
  */
 
-require APP_PATH . 'app/core/libraries/Alidayu/TopSdk.php';
-require APP_PATH . 'app/core/libraries/Alidayu/top/TopClient.php';
-require APP_PATH . 'app/core/libraries/Alidayu/top/request/AlibabaAliqinFcSmsNumSendRequest.php';
-date_default_timezone_set('Asia/Shanghai');
-
+include_once APPLICATION_PATH . '/../vendor/bmob/lib/BmobSms.class.php';
 use Phalcon\Cli\Task;
 use Phalcon\Di;
-
 use Multiple\Core\Constants\Services;
-
-
 class SmsTask extends Task
 {
-
     private $redis;
-
     public function mainAction()
     {
         echo "\nThis is the default task and the default action \n";
     }
-
     /**
      * @param array $params
      */
     public function sendAction()
     {
-
+        $sender = new BmobSms();
         $this->redis = Di::getDefault()->get(Services::REDIS);
-
         while (true) {
-
             $sms_json = $this->redis->rPop('sms');
             if(!$sms_json) {
                 sleep(2);
                 continue;
             }
-
             $sms = json_decode($sms_json, true);
             if (!isset($sms['mobile']) || trim($sms['mobile']) == '') {
                 echo "Error: mobile number is null \n";
                 sleep(2);
                 continue;
             }
-
             $mobile = $sms['mobile'];
-            $code = $sms['msg'];
-
+            $msg = $sms['msg'];
             echo "send $mobile push message \n";
-
-            $c = new \TopClient;
-            $c->appkey = '23428422';
-            $c->secretKey = 'baa9273882d94b1a7caad3e396341751';
-            $req = new \AlibabaAliqinFcSmsNumSendRequest;
-            $req->setExtend("123456");
-            $req->setSmsType("normal");
-            $req->setSmsFreeSignName("领骐物流");
-            $req->setSmsParam("{\"code\":\"$code\"}");
-            $req->setRecNum($mobile);
-            $req->setSmsTemplateCode("SMS_12991382");
-            $resp = $c->execute($req);
-
+            $sender->sendSms($mobile, $msg);
             echo "\n\n";
         }
     }
