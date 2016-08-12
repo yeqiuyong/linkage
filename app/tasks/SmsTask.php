0f12491abffb20978701bbc9433fb3d8c6d5f9b9
wangyang66 <646617779@qq.com>
2016-08-12 13:05:44 +0800

    edit

--- app/tasks/SmsTask.php
+++ app/tasks/SmsTask.php
@@ -7,39 +7,54 @@
  */
 
 include_once APPLICATION_PATH . '/../vendor/bmob/lib/BmobSms.class.php';
+
 use Phalcon\Cli\Task;
 use Phalcon\Di;
+
 use Multiple\Core\Constants\Services;
+
+
 class SmsTask extends Task
 {
+
     private $redis;
+
     public function mainAction()
     {
         echo "\nThis is the default task and the default action \n";
     }
+
     /**
      * @param array $params
      */
     public function sendAction()
     {
         $sender = new BmobSms();
+
         $this->redis = Di::getDefault()->get(Services::REDIS);
+
         while (true) {
+
             $sms_json = $this->redis->rPop('sms');
             if(!$sms_json) {
                 sleep(2);
                 continue;
             }
+
             $sms = json_decode($sms_json, true);
             if (!isset($sms['mobile']) || trim($sms['mobile']) == '') {
                 echo "Error: mobile number is null \n";
                 sleep(2);
                 continue;
             }
+
             $mobile = $sms['mobile'];
             $msg = $sms['msg'];
+
             echo "send $mobile push message \n";
+
             $sender->sendSms($mobile, $msg);
+
             echo "\n\n";
         }
     }
