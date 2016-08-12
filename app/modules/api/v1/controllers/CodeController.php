121631a1c0011e3f20a8fa1ae4b861d2746ff612
wangyang66 <646617779@qq.com>
2016-08-12 15:05:43 +0800

    edit sms

--- app/modules/api/v1/controllers/CodeController.php
+++ app/modules/api/v1/controllers/CodeController.php
@@ -60,7 +60,7 @@ class CodeController extends APIControllerBase
             //$this->alidayu->sendSMS($mobile, $verify_code);
             //return $this->respondError('a', $a);
             //send message
-            $this->sms->send($mobile, $verify_code,'');
+            $this->sms->send($mobile, $verify_code);
 
         }catch (Exception $e){
             return $this->respondError($e->getCode(), $e->getMessage());
