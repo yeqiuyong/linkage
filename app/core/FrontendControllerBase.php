9b91b0b137bbb98f9f3a592be7eb1db184c87761
wangyang66 <646617779@qq.com>
2016-08-17 13:11:48 +0800

    add cid,token

--- app/core/FrontendControllerBase.php
+++ app/core/FrontendControllerBase.php
@@ -13,8 +13,12 @@ use Multiple\Core\ControllerBase;
 class FrontendControllerBase extends ControllerBase
 {
 
+
     protected function initialize()
     {
+        $this->cid = $this->request->getPost('cid');
+        $this->token = $this->request->getPost('token');
+
         $this->tag->prependTitle('INVO | ');
         $this->view->setTemplateAfter('main');
     }
